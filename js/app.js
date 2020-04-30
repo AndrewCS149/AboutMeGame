e5af7f34e10d256c9c438c0e8ce368e51115cfb7
tekthree <tekthree@gmail.com>
2020-04-29 21:37:29 -0700

    added in the javascript

--- js/app.js
+++ js/app.js
@@ -1,14 +1,66 @@
-'use strict'
+'use strict';
 
-var watchedTigerKing = prompt('did you watch Tiger King?');
+//greeting
+alert('Hello');
+var username = prompt('Hi there, what\'s your name');
+// console.log('The users name is ' + username);
 
-// if they did watch Tiger King, I want to say 'Did Carol Baskin Kill her Husband?'
-// if they didn't watch Tiger King, I want to say 'Sorry'
+alert('Thanks for stopping by ' + username);
 
-if(watchedTigerKing.toLowerCase() === 'yes'){
-  alert('Did Carol Baskin Kill her Husband?');
-} else if(watchedTigerKing.toLowerCase() === 'no'){
-  alert('Sorry');
+//asking user if they want to play a game
+var guessingGame = prompt('would you be into playing a guessing game');
+
+if(guessingGame.toLowerCase() === 'yes' || guessingGame.toLowerCase() === 'y'){
+  alert('sweet let\'s go. I\'ll ask some questions and you answer (yes) or (no) or (y) or (n) as a reponse');
 } else {
-  prompt('Please answer with a yes or a no');
+  alert('Dang, maybe next time');
 }
+
+// Did i go to school in Japan
+
+var schoolJapan = prompt('Did I get lucky and have the chance to go to school in Japan?');
+
+if(schoolJapan.toLowerCase() === 'yes' || schoolJapan.toLowerCase() === 'y'){
+  console.log('correct');
+  alert('You know it! Such a great experience');
+} else if(schoolJapan.toLowerCase() === 'no' || schoolJapan.toLowerCase() === 'n'){
+  console.log('incorrect');
+  alert('I was there dude, read the bio');
+} else {
+  alert('answer yes or no');
+}
+
+// Did I join the military after highschool
+
+var questionMilitary = prompt('Did you read about me joining the military?');
+
+if(questionMilitary.toLowerCase() === 'yes' || questionMilitary.toLowerCase() === 'y'){
+  console.log('correct');
+  alert('Your on it, makes me feel so interesting');
+} else if(questionMilitary.toLowerCase() === 'no' || questionMilitary.toLowerCase() === 'n'){
+  console.log('incorrect');
+  alert('dang, you\'re not even paying attention huh');
+} else {
+  alert('answer yes or no');
+}
+
+//Is my current job graphic designer
+
+var questionDesigner = prompt('Did my bio tell you I am currently a graphic designer?');
+
+if(questionDesigner.toLowerCase() === 'yes' || questionDesigner.toLowerCase() === 'y'){
+  console.log('correct');
+  alert('Nice one, visual communications for the technical name');
+} else if (questionDesigner.toLowerCase() === 'no' || questionDesigner.toLowerCase() === 'n'){
+  console.log('incorrect');
+  alert('Come on man, give me my credit');
+} else {
+  alert('answer yes or no');
+}
+
+
+
+
+
+
+// (guessingGame.toLowerCase() === 'no' || guessingGame.toLowerCase() === 'n')
